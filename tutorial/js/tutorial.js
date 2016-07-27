fe07a4343f6074116949052ea0e3029bd2028318
toshi <me@toshimaru.net>
2016-07-28 01:49:28 +0900

    Add Comment

--- tutorial/js/tutorial.js
+++ tutorial/js/tutorial.js
@@ -15,7 +15,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
@@ -31,6 +32,18 @@ var CommentForm = React.createClass({
   }
 });
 
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
 
 ReactDOM.render(
